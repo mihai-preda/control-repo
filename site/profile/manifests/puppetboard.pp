dfb284d2c03fc162500b6984eef336c0a3f92e6a
Mihai Preda <mpreda@outlook.com>
2024-12-05 19:12:48 -0800

    modified ppboard to allow unauth

--- site/profile/manifests/puppetboard.pp
+++ site/profile/manifests/puppetboard.pp
@@ -17,4 +17,15 @@ class profile::puppetboard {
     vhost_name => 'web.preda.ca',
     port       => 80,
   }
+  puppet_authorization::rule { 'puppetlabs puppetdb metrics':
+    # Allow nodes to access the metrics service
+    # for puppetdb, the metrics service is the only
+    # service using the authentication service
+    match_request_path    => '/metrics',
+    match_request_type    => 'path',
+    match_request_method  => [get, post],
+    allow_unauthenticated => true,
+    sort_order            => 500,
+    path                  => '/etc/puppetlabs/puppetdb/conf.d/auth.conf',
+  }
 }
