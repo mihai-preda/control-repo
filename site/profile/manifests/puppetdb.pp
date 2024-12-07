fcba1d459b9871a6f87caacbf508e3e37af25a8f
Mihai Preda <mpreda@outlook.com>
2024-12-07 15:18:26 -0800

    added user passwd

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -4,7 +4,8 @@ class profile::puppetdb {
   class { 'puppetdb':
     postgresql_ssl_on       => true,
     database_host           => 'db.preda.ca',
-    database_listen_address => '0.0.0.0',
+    database_listen_address => '*',
+    database_password       => 'ctm3muf7tze!PYN@pvj',
   }
   exec { '/opt/puppetlabs/bin/puppetdb ssl-setup -f': }
 }
