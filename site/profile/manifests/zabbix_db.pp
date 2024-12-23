93fb18057ca36c26e1c97b1cc4e7537c7506c192
Mihai Preda <mpreda@outlook.com>
2024-12-23 15:40:06 -0800

    added zabbix_db and zabbix_server files

---
+++ site/profile/manifests/zabbix_db.pp
@@ -0,0 +1,15 @@
+# zabbix db server
+node 'zdb.preda.ca' {
+  class { 'mysql::server':
+    override_options => {
+      'mysqld' => {
+        'bind_address' => '172.16.10.19',
+      },
+    },
+  }
+  class { 'zabbix::database':
+    database_type => 'mysql',
+    zabbix_server => 'monitor.preda.ca',
+    zabbix_web    => 'monitor.preda.ca',
+  }
+}
--- site/profile/zabbix_server.pp
+++ site/profile/zabbix_server.pp
@@ -1,11 +1,15 @@
-# Class: profile::zabbix_server
+# zabbix server
 node 'monitor.preda.ca' {
   class { 'apache':
     mpm_module => 'prefork',
   }
-  class { 'zabbix':
+  class { 'apache::mod::php': }
+  class { 'mysql::client': }
+  class { 'zabbix::server': }
+  class { 'zabbix::web':
     zabbix_url    => 'monitor.preda.ca',
     database_type => 'mysql',
+    database_host => 'zdb.preda.ca',
   }
   if $facts['os']['selinux']['enabled'] {
     selboolean { ['httpd_can_network_connect', 'httpd_can_network_connect_db']:
