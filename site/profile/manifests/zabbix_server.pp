2969aaff44d254d49b8991a18b298a2168dd7f70
Mihai Preda <mpreda@outlook.com>
2024-12-23 21:13:03 -0800

    re-wrote zabbix_server

--- site/profile/manifests/zabbix_server.pp
+++ site/profile/manifests/zabbix_server.pp
@@ -1,18 +1,16 @@
 # zabbix server
 class profile::zabbix_server {
+  class { 'zabbix':
+    zabbix_url        => 'monitor.preda.ca',
+    database_type     => 'mysql',
+    database_user     => 'zabbix',
+    database_password => 'QuertY#1',
+  }
   class { 'apache':
     mpm_module => 'prefork',
   }
   class { 'apache::mod::php': }
-  class { 'mysql::client':
-    package_name    => 'mariadb-client',
-    bindings_enable => true,
-  }
-  class { 'zabbix::server': }
   class { 'zabbix::web':
-    zabbix_url    => 'monitor.preda.ca',
-    database_type => 'mysql',
-    database_host => 'zdb.preda.ca',
   }
   if $facts['os']['selinux']['enabled'] {
     selboolean { ['httpd_can_network_connect', 'httpd_can_network_connect_db']:
