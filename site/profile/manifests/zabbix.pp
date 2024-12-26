280f5fc05d1851ca7b64557e3eb936b6faf65528
Mihai Preda <mpreda@outlook.com>
2024-12-26 13:16:21 -0800

    replaced php-fpm with ssl

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -6,7 +6,7 @@ class profile::zabbix {
     database_user     => 'zabbix',
     database_password => 'Burninator@1',
   }
-  class { 'apache::mod::php-fpm': }
+  class { 'apache::mod::ssl': }
   class { 'mysql::client': }
   class { 'zabbix::server':
     database_host => 'zdb.preda.ca',
