cfbf52cc854d7d5851f03af4a276c662ed64e557
Mihai Preda <mpreda@outlook.com>
2024-12-30 13:38:35 -0800

    removed timezone

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -9,9 +9,8 @@ class profile::zabbix {
   class { 'apache::mod::ssl': }
   class { 'mysql::client': }
   class { 'zabbix::server':
-    database_host   => 'zdb.preda.ca',
-    database_type   => 'mysql',
-    zabbix_timezone => 'America/Vancouver',
-    zabbix_version  => '7.0',
+    database_host  => 'zdb.preda.ca',
+    database_type  => 'mysql',
+    zabbix_version => '7.0',
   }
 }
