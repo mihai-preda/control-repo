ebbfaece688c7d656155fbe38b1ce871a77d2b8b
Mihai Preda <mpreda@outlook.com>
2024-12-30 13:06:22 -0800

    reverted to zabbix 7.0

--- site/profile/manifests/zabbix_agent.pp
+++ site/profile/manifests/zabbix_agent.pp
@@ -1,7 +1,7 @@
 # zabbix agent
 class profile::zabbix_agent {
   class { 'zabbix::agent':
-    zabbix_version  => '7.2',
+    zabbix_version  => '7.0',
     server          => 'monitor.preda.ca',
     manage_firewall => true,
   }
