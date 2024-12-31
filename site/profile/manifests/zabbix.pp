9c756392f16a3bff7b7c47a5d60f0af52dbc2054
Mihai Preda <mpreda@outlook.com>
2024-12-30 17:43:18 -0800

    added manage repo var and zb ver

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -5,15 +5,13 @@ class profile::zabbix {
     database_type     => 'mysql',
     database_user     => 'zabbix',
     database_password => 'Burninator@1',
+    manage_repo       => true,
+    zabbix_version    => '7.0',
   }
   class { 'apache::mod::ssl': }
   class { 'mysql::client': }
   class { 'zabbix::server':
     database_host => 'zdb.preda.ca',
     database_type => 'mysql',
-  }
-  class { 'zabbix::repo':
-    zabbix_version => 7.0,
-    manage_repo    => true,
   }
 }
