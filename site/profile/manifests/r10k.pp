2d01d5b774da2936ca47b70bd1850664142c75b0
Mihai Preda <mpreda@outlook.com>
2024-12-30 22:03:09 -0800

    modified r10k with code from r10k repo

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -5,10 +5,11 @@ class profile::r10k {
   }
   class { 'r10k::webhook::config':
     use_mcollective => false,
-    enable_ssl      => false,
   }
+
   class { 'r10k::webhook':
-    user  => 'root',
-    group => 'root',
+    ensure => true,
+    server => {
+      protected => false,
+    },
   }
-}
