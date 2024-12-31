e9d3741e65af7e26189158cc389ce96b0959ba8f
Mihai Preda <mpreda@outlook.com>
2024-12-30 18:28:09 -0800

    added webhook with r10k

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,14 @@
+# web hook with r10k
+class profile::r10k {
+  class { 'r10k':
+    remote => 'https://github.com/mihai-preda/control-repo.git',
+  }
+  class { 'r10k::webhook::config':
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class { 'r10k::webhook':
+    user  => 'root',
+    group => 'root',
+  }
+}
--- site/role/manifests/puppetserver.pp
+++ site/role/manifests/puppetserver.pp
@@ -3,4 +3,5 @@ class role::puppetserver {
   include profile::base
   include profile::puppetserver
   include profile::zabbix_agent
+  include profile::r10k
 }
