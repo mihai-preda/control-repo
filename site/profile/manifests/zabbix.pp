fb75df7cb9968c418dbbc7d646a78c0b1282f2a3
Mihai Preda <mpreda@outlook.com>
2025-01-15 15:39:25 -0800

    added vshost block

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -14,4 +14,11 @@ class profile::zabbix {
     database_host => 'zdb.preda.ca',
     database_type => 'mysql',
   }
+  apache::vhost { 'default-ssl':
+    port              => 443,
+    ssl               => true,
+    default_ssl_cert  => '/etc/pki/tls/certs/cert.pem',
+    default_ssl_key   => '/etc/pki/tls/private/privatekey.pem',
+    default_ssl_chain => '/etc/pki/tls/certs/chain.pem',
+  }
 }
