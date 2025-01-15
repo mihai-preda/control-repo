2251d3b54ae464fb3b550d0b201b1f6a99d184e0
Mihai Preda <mpreda@outlook.com>
2025-01-15 15:48:33 -0800

    moved config from apache class to apache::modssl class

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -8,17 +8,16 @@ class profile::zabbix {
     manage_repo       => true,
     zabbix_version    => '6.0',
   }
-  class { 'apache::mod::ssl': }
-  class { 'mysql::client': }
-  class { 'zabbix::server':
-    database_host => 'zdb.preda.ca',
-    database_type => 'mysql',
-  }
-  class { 'apache':
+  class { 'apache::mod::ssl':
     port              => 443,
     ssl               => true,
     default_ssl_cert  => '/etc/pki/tls/certs/cert.pem',
     default_ssl_key   => '/etc/pki/tls/private/privatekey.pem',
     default_ssl_chain => '/etc/pki/tls/certs/chain.pem',
+  }
+  class { 'mysql::client': }
+  class { 'zabbix::server':
+    database_host => 'zdb.preda.ca',
+    database_type => 'mysql',
   }
 }
