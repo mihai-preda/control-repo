ce77872f9a66cdb1cc93b52f381fde322e3eda0a
Mihai Preda <mpreda@outlook.com>
2025-01-15 18:29:10 -0800

    added multiple variables

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -1,17 +1,17 @@
 # zabbix server
 class profile::zabbix {
   class { 'zabbix::web':
+    manage_vhost      => true,
     zabbix_url        => 'monitor.preda.ca',
     database_type     => 'mysql',
     database_user     => 'zabbix',
     database_password => 'Burninator@1',
     manage_repo       => true,
     zabbix_version    => '6.0',
-    port              => 443,
-    ssl               => true,
-    default_ssl_cert  => '/etc/pki/tls/certs/cert.pem',
-    default_ssl_key   => '/etc/pki/tls/private/privatekey.pem',
-    default_ssl_chain => '/etc/pki/tls/certs/chain.pem',
+    apache_use_ssl    => true,
+    apache_ssl_cert   => '/etc/pki/tls/certs/cert.pem',
+    apache_ssl_key    => '/etc/pki/tls/private/privatekey.pem',
+    apache_ssl_chain  => '/etc/pki/tls/certs/chain.pem',
   }
   class { 'apache::mod::ssl': }
   class { 'mysql::client': }
