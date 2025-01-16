05dc910abae2fe45403622d9f21d30245708a450
Mihai Preda <mpreda@outlook.com>
2025-01-15 18:12:48 -0800

    moved variables around

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -7,14 +7,13 @@ class profile::zabbix {
     database_password => 'Burninator@1',
     manage_repo       => true,
     zabbix_version    => '6.0',
-  }
-  class { 'apache':
     port              => 443,
     ssl               => true,
     default_ssl_cert  => '/etc/pki/tls/certs/cert.pem',
     default_ssl_key   => '/etc/pki/tls/private/privatekey.pem',
     default_ssl_chain => '/etc/pki/tls/certs/chain.pem',
   }
+  class { 'apache::mod::ssl': }
   class { 'mysql::client': }
   class { 'zabbix::server':
     database_host => 'zdb.preda.ca',
