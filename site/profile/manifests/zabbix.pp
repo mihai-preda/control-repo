58717a4bbe9b71cb561487b4e8c2a35f31368b94
Mihai Preda <mpreda@outlook.com>
2025-01-15 19:14:00 -0800

    modified databse_host var location

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -3,6 +3,7 @@ class profile::zabbix {
   class { 'zabbix::web':
     manage_vhost      => true,
     zabbix_url        => 'monitor.preda.ca',
+    database_host     => 'zdb.preda.ca',
     database_type     => 'mysql',
     database_user     => 'zabbix_server',
     database_password => 'Burninator@1',
@@ -15,8 +16,5 @@ class profile::zabbix {
   }
   class { 'apache::mod::ssl': }
   class { 'mysql::client': }
-  class { 'zabbix::server':
-    database_host => 'zdb.preda.ca',
-    database_type => 'mysql',
-  }
+  class { 'zabbix::server': }
 }
