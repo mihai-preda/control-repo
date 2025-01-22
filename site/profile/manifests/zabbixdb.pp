c0f9337f2d4176cf9a6a078740df1a4e3d20c3be
Mihai Preda <mpreda@outlook.com>
2025-01-21 16:13:08 -0800

    added db user and password to db.pp

--- site/profile/manifests/zabbixdb.pp
+++ site/profile/manifests/zabbixdb.pp
@@ -4,8 +4,10 @@ class profile::zabbixdb {
     listen_addresses => '172.16.10.14',
   }
   class { 'zabbix::database':
-    database_type    => 'postgresql',
-    zabbix_web_ip    => '172.16.10.13',
-    zabbix_server_ip => '172.16.10.13',
+    database_type     => 'postgresql',
+    zabbix_web_ip     => '172.16.10.13',
+    zabbix_server_ip  => '172.16.10.13',
+    database_user     => 'zabbix_user',
+    database_password => 'Burninator@1',
   }
 }
