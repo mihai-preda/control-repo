8e3e227b6785605750d52d7ca3d8f2888090dfe9
Mihai Preda <mpreda@outlook.com>
2025-01-21 19:34:58 -0800

    added db variables

--- site/profile/manifests/zabbixdb.pp
+++ site/profile/manifests/zabbixdb.pp
@@ -1,14 +1,15 @@
 # zabbix db server
 class profile::zabbixdb {
   class { 'postgresql::server':
-    postgresql_ssl_on => true,
     listen_addresses  => '172.16.10.14',
   }
   class { 'zabbix::database':
     database_type     => 'postgresql',
+    manage_database   => true,
     zabbix_web_ip     => '172.16.10.13',
     zabbix_server_ip  => '172.16.10.13',
     database_user     => 'zabbix_user',
     database_password => 'Burninator@1',
+    database_name     => 'zabbixdb',
   }
 }
