a13955cf14ec273d6a2a233c804accf8354831bb
Mihai Preda <mpreda@outlook.com>
2025-02-08 20:31:04 -0800

    removed mysql module

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,18 +3,6 @@ class profile::base {
   exec { 'set locale':
     command => '/bin/localectl set-locale LANG=en_GB',
   }
-  # service { 'NetworkManager':
-  #   ensure => 'stopped',
-  #   enable => 'false',
-  # }
-  # network_route { '172.16.1.0/24':
-  #   ensure    => 'present',
-  #   gateway   => '172.16.1.1',
-  #   interface => 'eth1',
-  #   netmask   => '255.255.255.0',
-  #   network   => '172.16.1.0',
-  #   options   => 'table 200',
-  # }
   user { 'mihai':
     ensure => 'present',
     groups => ['wheel'],
