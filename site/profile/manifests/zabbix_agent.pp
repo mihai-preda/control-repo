b9847e09a94d19d33b6bcbba85e83648fb79283c
Mihai Preda <mpreda@outlook.com>
2025-02-15 14:55:49 -0800

    added zabbix_agent2 for windows

--- site/profile/manifests/zabbix_agent.pp
+++ site/profile/manifests/zabbix_agent.pp
@@ -5,4 +5,21 @@ class profile::zabbix_agent {
     server          => 'monitor.preda.ca',
     manage_firewall => true,
   }
+  $tmpdir = $facts['windows_env']['TMP'];
+
+  download_file { 'get zabbix-installer.msi':
+    url                   => "https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.38/zabbix_agent2_plugins-6.0.38-windows-amd64.msi",
+    destination_directory => $tmpdir,
+    destination_file      => 'zabbix_agent-windows-amd64-openssl.msi',
+  }
+
+  class { 'zabbix::agent':
+    zabbix_version          => $zabbix_version,
+    manage_resources        => true,
+    manage_choco            => false,
+    zabbix_package_agent    => 'Zabbix Agent (64-bit)',
+    zabbix_package_state    => present,
+    zabbix_package_provider => 'windows',
+    zabbix_package_source   => "${tmpdir}/zabbix_agent-windows-amd64-openssl.msi",
+  }
 }
