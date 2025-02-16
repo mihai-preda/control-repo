bb1274a95aa85ad10ffc94f8cdf80baca4296556
Mihai Preda <mpreda@outlook.com>
2025-02-15 16:25:35 -0800

    edited zabbix_agent for windows

--- site/profile/manifests/zabbix_agent.pp
+++ site/profile/manifests/zabbix_agent.pp
@@ -8,9 +8,9 @@ class profile::zabbix_agent {
   $tmpdir = $facts['windows_env']['TMP'];
 
   download_file { 'get zabbix-installer.msi':
-    url                   => "https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.38/zabbix_agent2_plugins-6.0.38-windows-amd64.msi",
+    url                   => 'https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.38/zabbix_agent2_plugins-6.0.38-windows-amd64.msi',
     destination_directory => $tmpdir,
-    destination_file      => 'zabbix_agent-windows-amd64-openssl.msi',
+    destination_file      => 'zabbix_agent2_plugins-6.0.38-windows-amd64.msi',
   }
 
   class { 'zabbix::agent':
@@ -20,6 +20,6 @@ class profile::zabbix_agent {
     zabbix_package_agent    => 'Zabbix Agent (64-bit)',
     zabbix_package_state    => present,
     zabbix_package_provider => 'windows',
-    zabbix_package_source   => "${tmpdir}/zabbix_agent-windows-amd64-openssl.msi",
+    zabbix_package_source   => "${tmpdir}/zabbix_agent2_plugins-6.0.38-windows-amd64.msi",
   }
 }
