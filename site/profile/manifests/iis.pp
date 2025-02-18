93662685d84a8dba1e0fed5ebfe3cbe4d868110c
Mihai Preda <mpreda@outlook.com>
2025-02-17 20:40:31 -0800

    fixed typo

--- site/profile/manifests/iis.pp
+++ site/profile/manifests/iis.pp
@@ -1,6 +1,6 @@
 # IIS profile
 class profile::iis {
-  $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework--ASPNET',
+  $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework-ASPNET',
   'WAS-NET-Environment','Web-Http-Redirect','Web-Filtering','Web-Mgmt-Console','Web-Mgmt-Tools']
   windowsfeature { $iis_features:
     ensure => present,
