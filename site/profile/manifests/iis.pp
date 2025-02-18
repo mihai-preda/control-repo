1ad8731370b77cdd6c47e2a63fc335ce0f1d1af0
Mihai Preda <mpreda@outlook.com>
2025-02-17 20:42:17 -0800

    removed all the features and added variables

--- site/profile/manifests/iis.pp
+++ site/profile/manifests/iis.pp
@@ -1,8 +1,8 @@
 # IIS profile
 class profile::iis {
-  $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework-ASPNET',
-  'WAS-NET-Environment','Web-Http-Redirect','Web-Filtering','Web-Mgmt-Console','Web-Mgmt-Tools']
-  windowsfeature { $iis_features:
-    ensure => present,
+  windowsfeature { 'Web-WebServer':
+    ensure                 => present,
+    installmanagementtools => true,
+    installsubfeatures     => true,
   }
 }
