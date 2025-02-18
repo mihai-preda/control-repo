84b1161276b54b08366b3e50c16a33144df7e309
Mihai Preda <mpreda@outlook.com>
2025-02-17 20:39:09 -0800

    removed 48 from name of win features

--- site/profile/manifests/iis.pp
+++ site/profile/manifests/iis.pp
@@ -1,6 +1,6 @@
 # IIS profile
 class profile::iis {
-  $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net48','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework-48-ASPNET',
+  $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework--ASPNET',
   'WAS-NET-Environment','Web-Http-Redirect','Web-Filtering','Web-Mgmt-Console','Web-Mgmt-Tools']
   windowsfeature { $iis_features:
     ensure => present,
