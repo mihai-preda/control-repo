3c7c8819b908670d986b28868cb42fdc5d0ca5ff
Mihai Preda <mpreda@outlook.com>
2025-03-09 15:10:50 -0700

    added zabbix_server_name var

--- site/profile/manifests/zabbix.pp
+++ site/profile/manifests/zabbix.pp
@@ -1,20 +1,21 @@
 # zabbix server profile
 class profile::zabbix {
   class { 'zabbix::web':
-    manage_vhost      => true,
-    zabbix_url        => 'monitor.preda.ca',
-    database_name     => 'zabbixdb',
-    database_user     => 'zabbix_user',
-    database_password => 'Burninator@1',
-    zabbix_timezone   => 'America/Vancouver',
-    manage_repo       => true,
-    zabbix_version    => '6.0',
-    apache_use_ssl    => true,
-    database_host     => 'db.preda.ca',
-    database_type     => 'postgresql',
-    apache_ssl_cert   => '/etc/pki/tls/certs/cert.pem',
-    apache_ssl_key    => '/etc/pki/tls/private/privatekey.pem',
-    apache_ssl_chain  => '/etc/pki/tls/certs/chain.pem',
+    manage_vhost       => true,
+    zabbix_server_name => '',
+    zabbix_url         => 'monitor.preda.ca',
+    database_name      => 'zabbixdb',
+    database_user      => 'zabbix_user',
+    database_password  => 'Burninator@1',
+    zabbix_timezone    => 'America/Vancouver',
+    manage_repo        => true,
+    zabbix_version     => '6.0',
+    apache_use_ssl     => true,
+    database_host      => 'db.preda.ca',
+    database_type      => 'postgresql',
+    apache_ssl_cert    => '/etc/pki/tls/certs/cert.pem',
+    apache_ssl_key     => '/etc/pki/tls/private/privatekey.pem',
+    apache_ssl_chain   => '/etc/pki/tls/certs/chain.pem',
   }
   class { 'apache::mod::ssl': }
   class { 'postgresql::client': }
