6e8491a0de1e0a5505ed7cd6d8f95775015c747e
Mihai Preda <mpreda@outlook.com>
2025-03-10 20:52:26 -0700

    renamed physical server

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,5 +5,5 @@ node 'puppet.preda.ca' { include role::puppetserver }
 node 'web.preda.ca' { include role::webserver }
 node 'monitor.preda.ca' { include role::zabbix }
 node 'zdb.preda.ca' { include role::zabbixdb }
-node 'win-tan06jp8l69.preda.ca' { include role::iis }
-node 'phys-srv-01.preda.ca' { include role::sp1 }
+node 'red.preda.ca' { include role::iis }
+node 'mini.preda.ca' { include role::sp1 }
