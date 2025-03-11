190e6d8a5a00613978d49be95af3e69e54d16bbc
Mihai Preda <mpreda@outlook.com>
2025-03-11 15:21:33 -0700

    modified node_purge_ttl to 1d

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -6,7 +6,7 @@ class profile::puppetdb {
     database_host           => 'db.preda.ca',
     database_listen_address => '0.0.0.0',
     database_password       => 'ctm3muf7tze!PYN@pvj',
-    node_purge_ttl          => '3',
+    node_purge_ttl          => '1d',
   }
   exec { '/opt/puppetlabs/bin/puppetdb ssl-setup -f': }
   firewall { '100 allow http and https access':
