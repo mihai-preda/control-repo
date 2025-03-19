82bdd5ffa2ad1f51692e8f4fbb32cba584232bb7
Mihai Preda <mpreda@outlook.com>
2025-03-18 20:15:52 -0700

    changes to puppetdb node tll and purge ttl

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -6,8 +6,8 @@ class profile::puppetdb {
     database_host           => 'db.preda.ca',
     database_listen_address => '0.0.0.0',
     database_password       => 'ctm3muf7tze!PYN@pvj',
-    node_ttl                => '0s',
-    node_purge_ttl          => '0s',
+    node_ttl                => '1h',
+    node_purge_ttl          => '2h',
   }
   exec { '/opt/puppetlabs/bin/puppetdb ssl-setup -f': }
   firewall { '100 allow http and https access':
