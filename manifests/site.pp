7a33e8308defa87e483a8fe2df0c742dc4a6f31a
Mihai Preda <mpreda@outlook.com>
2025-03-22 10:50:02 -0700

    modified site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,5 +5,5 @@ node 'puppet.preda.ca' { include role::puppetserver }
 node 'web.preda.ca' { include role::webserver }
 node 'monitor.preda.ca' { include role::zabbix }
 node 'zdb.preda.ca' { include role::zabbixdb }
-node 'red.preda.ca' { include role::iis }
+node 'win-38njq8tg.preda.ca' { include role::iis }
 node 'mini.preda.ca' { include role::sp1 }
