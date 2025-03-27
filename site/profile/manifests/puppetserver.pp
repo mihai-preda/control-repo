667ffbfba294db5f3341d5844146025ddefea4d2
Mihai Preda <mpreda@outlook.com>
2025-03-27 00:12:55 -0700

    modified hiera class

--- site/profile/manifests/puppetserver.pp
+++ site/profile/manifests/puppetserver.pp
@@ -23,12 +23,11 @@ class profile::puppetserver {
     manage_routes           => true,
   }
   class { 'hiera':
-    hiera_version        => '5',
-    hiera5_defaults      => { 'datadir' => 'data', 'data_hash' => 'yaml_data' },
     hierarchy            => [
-      { 'name' => 'Virtual yaml', 'path'  => 'virtual/%{virtual}.yaml' },
-      { 'name' => 'Nodes yaml', 'paths' => ['nodes/%{trusted.certname}.yaml', 'nodes/%{osfamily}.yaml'] },
-      { 'name' => 'Default yaml file', 'path' => 'common.yaml' },
+      'nodes/%{clientcert}',
+      'locations/%{location}',
+      'environments/%{applicationtier}',
+      'common',
     ],
     eyaml                => true,
     eyaml_gpg            => false,
