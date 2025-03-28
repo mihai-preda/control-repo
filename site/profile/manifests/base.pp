f15f0e1d6ab89be9abad0eb230eca19da997732a
Mihai Preda <mpreda@outlook.com>
2025-03-27 23:00:10 -0700

    added ssh key

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -26,7 +26,7 @@ class profile::base {
     ensure => present,
     user   => 'mihai',
     type   => 'ssh-ed25519',
-    key    => 'ENC[PKCS7,MIIBuQYJKoZIhvcNAQcDoIIBqjCCAaYCAQAxggEhMIIBHQIBADAFMAACAQEwDQYJKoZIhvcNAQEBBQAEggEAYOrN56hGTAqMFbb5RJYoN5KciT9Ant3xX+lrCs3b/DJiu50LXXbXCD3saPK2ceYz+VM/OX79910CBGM/gySr55Q8RG6RFW/Yn2HQ2YwA1oACIToISZy4BkwUWGHAyQQ0vRgixUGWY2MCFkyYZtjCYP/hwF1icES0h5q885S8/j9h1agbGOk6fqOzoP8GEEtdGP8usE8bcXXckzHlBHGM8J9M3JkWPDwpbvOOsxxgoUPo3MU4pQCKV+t9lmDbz5NrG4aNj99zh1Ldchg//i1jov95Iet9zv8NRxtCU6c0XtoIDft062CTYztXuP63zw2n23fvu+fLt7XI5e/OIqKWEjB8BgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBBMDriSaIF1oqJDXQFVo7trgFCsvJHk0dd9Sntig57+WGprH/XxrlcioTMUtpwUds3EEb92nntDtM/X5nmJv3g4h40Qtcm9xbQnnPVHpGDhjunTzrsDKEToMveV18ZHim3Opg==]',
+    key    => 'AAAAC3NzaC1lZDI1NTE5AAAAIFNKJ4PExMq163zYebUxFPhEasnbEuNCwgNh7SjwQg18',
   }
   notify { 'hello from the puppet server':
   }
