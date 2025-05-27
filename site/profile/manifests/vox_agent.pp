19aaf16d74fd5654692d0070ddf021dca9929ca5
Mihai Preda <mpreda@outlook.com>
2025-05-26 23:14:49 -0700

    removed openvox-release from vox_agent

--- site/profile/manifests/vox_agent.pp
+++ site/profile/manifests/vox_agent.pp
@@ -1,10 +1,5 @@
 # install openvox agent
 class profile::vox_agent {
-  package { 'opevox8-release':
-    ensure   => 'present',
-    provider => rpm,
-    source   => 'https://yum.voxpupuli.org/openvox8-release-fedora-41.noarch.rpm',
-  }
   package { 'openvox-agent':
     ensure => 'present',
   }
