bb191c36bfba0152b8df6dab4df6d9467f24a2b5
Mihai Preda <mpreda@outlook.com>
2025-05-26 23:10:06 -0700

    added vox_agent manifest file

--- site/profile/manifests/vox_agent.pp
+++ site/profile/manifests/vox_agent.pp
@@ -1,17 +1,8 @@
 # install openvox agent
 class profile::vox_agent {
-  file { '/etc/yum.repos.d/openvox8-release.repo':
-    ensure  => file,
-    content => "[openvox8]
-name=OpenVox 8 Repository fedora 41 - ${'basearch'}
-baseurl=https://yum.voxpupuli.org/openvox8/fedora/41/${'basearch'}
-gpgkey=file:///etc/pki/rpm-gpg/GPG-KEY-openvox-openvox8-release
-enabled=1
-gpgcheck=1",
-  }
-
-# install openvox agent
-  package { 'openvox_agent':
-    ensure => 'latest',
+  package { 'opevox8-release':
+    ensure   => 'present',
+    provider => rpm,
+    source   => 'https://yum.voxpupuli.org/openvox8-release-fedora-41.noarch.rpm',
   }
 }
