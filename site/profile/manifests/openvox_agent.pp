a124fb872a814a9c0d1a0de457d90f47e8f5385c
Mihai Preda <mpreda@outlook.com>
2025-07-01 21:28:48 -0700

    renamed var

--- site/profile/manifests/openvox_agent.pp
+++ site/profile/manifests/openvox_agent.pp
@@ -1,4 +1,4 @@
 # profile puppet agent
 class profile::openvox_agent {
-  class { 'puppet': puppet_runinterval => '2h',  agent_server_hostname => 'puppet.preda.ca' }
+  class { 'puppet': runinterval => '2h',  agent_server_hostname => 'puppet.preda.ca' }
 }
