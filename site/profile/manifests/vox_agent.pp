9ed9df6a10dddfe984832a8d22874a60b10cd5e0
Mihai Preda <mpreda@outlook.com>
2025-07-01 19:36:43 -0700

    removed vox from puppet and added it to vox_agent

--- site/profile/manifests/vox_agent.pp
+++ site/profile/manifests/vox_agent.pp
@@ -1,6 +1,15 @@
 # install openvox agent
 class profile::vox_agent {
-  package { 'openvox-agent':
+  include yum
+  $release=8
+  $os_name = $facts['os']['name'] ? {
+    'Fedora' => 'fedora',
+    'Amazon' => 'amazon',
+    default  => 'el',
+  }
+
+  yum::install { "openvox${release}-release":
     ensure => 'present',
+    source => "https://yum.voxpupuli.org/openvox${release}-release-${os_name}-${facts['os']['release']['major']}.noarch.rpm",
   }
 }
