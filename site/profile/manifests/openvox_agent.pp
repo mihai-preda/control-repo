911f0d4bbde742d7d004d47cb0bdd16a7d1684fc
Mihai Preda <mpreda@outlook.com>
2025-07-01 21:45:36 -0700

    removed all openvox_agent files

--- site/profile/manifests/openvox_agent.pp
+++
@@ -1,4 +0,0 @@
-# profile puppet agent
-class profile::openvox_agent {
-  class { 'puppet': runmode => 'systemd.timer', runinterval => '2h',  agent_server_hostname => 'puppet.preda.ca' }
-}
--- site/role/manifests/sp1.pp
+++ site/role/manifests/sp1.pp
@@ -1,6 +1,5 @@
 # role server physical 1
 class role::sp1 {
-  include profile::openvox_agent
   include profile::resolver
   #include profile::zabbix_agent
 }
