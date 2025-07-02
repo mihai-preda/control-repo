d40f0133e29af3c729c603204ba8b8f73e7589d3
Mihai Preda <mpreda@outlook.com>
2025-07-01 19:34:05 -0700

    added openvox to puppet agent

--- site/profile/manifests/puppet_agent.pp
+++ site/profile/manifests/puppet_agent.pp
@@ -5,4 +5,16 @@ class profile::puppet_agent {
     collection      => 'puppet8',
     config          => [{ section => main, setting => runinterval, value => '1h' }],
   }
+  include yum
+  $release=8
+  $os_name = $facts['os']['name'] ? {
+    'Fedora' => 'fedora',
+    'Amazon' => 'amazon',
+    default  => 'el',
+  }
+
+  yum::install { "openvox${release}-release":
+    ensure => 'present',
+    source => "https://yum.voxpupuli.org/openvox${release}-release-${os_name}-${facts['os']['release']['major']}.noarch.rpm",
+  }
 }
