f2efaf3427f013b43703e8cdbb8d21b79d3b097b
Mihai Preda <mpreda@outlook.com>
2025-07-05 07:18:32 -0700

    modified hiera structure

--- site/profile/manifests/puppetserver.pp
+++ site/profile/manifests/puppetserver.pp
@@ -26,7 +26,8 @@ class profile::puppetserver {
     hiera_version   => '5',
     hiera5_defaults => { 'datadir' => 'data', 'data_hash' => 'yaml_data' },
     hierarchy       => [
-      { 'name' => 'Nodes yaml', 'paths' => ['nodes/%{trusted.certname}.yaml', 'nodes/%{osfamily}.yaml'] },
+      { 'name' => 'Nodes yaml', 'path' => 'nodes/%{trusted.certname}.yaml' },
+      { 'name' => 'OSFamily.yaml', 'path' => 'os/%{osfamily}.yaml' },
       { 'name' => 'Default yaml file', 'path' => 'common.yaml' },
     ],
   }
