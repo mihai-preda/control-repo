dfda448c4185d09ce8d5849478a0f746628e70c4
Mihai Preda <mpreda@outlook.com>
2025-07-05 08:04:10 -0700

    reverted hiera config to default

--- site/profile/manifests/puppetserver.pp
+++ site/profile/manifests/puppetserver.pp
@@ -26,8 +26,8 @@ class profile::puppetserver {
     hiera_version   => '5',
     hiera5_defaults => { 'datadir' => 'data', 'data_hash' => 'yaml_data' },
     hierarchy       => [
-      { 'name' => 'Nodes yaml', 'path' => 'nodes/%{trusted.certname}.yaml' },
-      { 'name' => 'OSFamily.yaml', 'path' => 'os/%{os.family}.yaml' },
+      { 'name' => 'Virtual yaml', 'path'  => 'virtual/%{virtual}.yaml' },
+      { 'name' => 'Nodes yaml', 'paths' => ['nodes/%{trusted.certname}.yaml', 'nodes/%{osfamily}.yaml'] },
       { 'name' => 'Default yaml file', 'path' => 'common.yaml' },
     ],
   }
