2ddfd991c2046412e7b9c68fd45c15183137ac15
Mihai Preda <mpreda@outlook.com>
2025-07-05 22:09:36 -0700

    modified openssl class

--- site/profile/manifests/ssc.pp
+++ site/profile/manifests/ssc.pp
@@ -1,4 +1,8 @@
 # certificates
+class { 'openssl':
+  package_ensure         => latest,
+  ca_certificates_ensure => latest,
+}
 openssl::certificate::x509 { 'app1-srv':
   ensure       => present,
   country      => 'CA',
--- site/role/manifests/app1.pp
+++ site/role/manifests/app1.pp
@@ -1,6 +1,6 @@
 # app1 role
 class role::app1 {
   include profile::tomcat
-  contain openssl::certificate::x509
+  contain openssl
   include role::default
 }
