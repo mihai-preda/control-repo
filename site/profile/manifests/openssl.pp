e938cebc13184fd60a4629a4d78cee425bdfe2cf
Mihai Preda <mpreda@outlook.com>
2025-07-06 23:40:51 -0700

    used a class profile

--- site/profile/manifests/openssl.pp
+++ site/profile/manifests/openssl.pp
@@ -1,8 +1,10 @@
 # openssl
-class { 'openssl':
-  package_ensure         => latest,
-  ca_certificates_ensure => latest,
-}
-openssl::certificate::x509 { 'hostcert':
-  commonname => $facts['networking']['fqdn'],
+class profile::openssl {
+  class { 'openssl':
+    package_ensure         => latest,
+    ca_certificates_ensure => latest,
+  }
+  openssl::certificate::x509 { 'hostcert':
+    commonname => $facts['networking']['fqdn'],
+  }
 }
--- site/role/manifests/app1.pp
+++ site/role/manifests/app1.pp
@@ -1,6 +1,6 @@
 # app1 role
 class role::app1 {
   include profile::tomcat
-  include openssl
+  include profile::openssl
   include role::default
 }
