55c10726377e62a3280a0fe014b2e6d663647310
Mihai Preda <mpreda@outlook.com>
2025-07-17 20:23:14 -0700

    re-enabled tomcat config

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -9,38 +9,41 @@ class profile::tomcat {
   package { 'java-17-openjdk-headless':
     ensure => 'present',
   }
-  # $keystore_path = '/etc/pki/tls/certs'
-  # $fqdn = $facts['networking']['fqdn']
-  # $ssl_dir = '/etc/pki/tls'
-  # $keystore_source = "${ssl_dir}/private/${fqdn}.p12"
-  # file { $keystore_path:
-  #   ensure   => file,
-  #   source   => $keystore_source,
-  #   #owner          => $keystore_user,
-  #   mode     => '0400',
-  #   checksum => 'md5',
-  #   #checksum_value => $keystore_checksum,
-  # }
+  $keystore_path = '/etc/pki/tls/certs'
+  $fqdn = $facts['networking']['fqdn']
+  $ssl_dir = '/etc/pki/tls'
+  $keystore_source = "${ssl_dir}/private/${fqdn}.p12"
+  $keystore_pass = '1k0eop3l0-2jd]3hh7'
+  file { $keystore_path:
+    ensure         => file,
+    source         => $keystore_source,
+    owner          => $keystore_user,
+    mode           => '0400',
+    checksum       => 'md5',
+    checksum_value => $keystore_checksum,
+  }
 
-  # -> tomcat::config::server::connector { 'default-https':
-  #   catalina_base         => $catalina_base,
-  #   port                  => $https_port,
-  #   protocol              => $http_version,
-  #   purge_connectors      => true,
-  #   cert_key_file         => "${ssl_dir}/certs/${fqdn}.key",
-  #   cert_file             => "${ssl_dir}/certs/${fqdn}.crt",
-  #   #cert_chain_file       => "${ssl_dir}/certs/",
-  #   cert_type             => 'RSA',
-  #   additional_attributes => {
-  #     'SSLEnabled'          => bool2str($https_enabled),
-  #     'maxThreads'          => $https_connector_max_threads,
-  #     'scheme'              => $https_connector_scheme,
-  #     'secure'              => bool2str($https_connector_secure),
-  #     'clientAuth'          => bool2str($https_connector_client_auth),
-  #     'sslProtocol'         => $https_connector_ssl_protocol,
-  #     'sslEnabledProtocols' => join($https_connector_ssl_protocols_enabled, ','),
-  #     'ciphers'             => join($ciphers_enabled, ','),
+  -> tomcat::config::server::connector { 'default-https':
+    catalina_base         => $catalina_base,
+    port                  => $https_port,
+    protocol              => $http_version,
+    purge_connectors      => true,
+    cert_key_file         => "${ssl_dir}/certs/${fqdn}.key",
+    cert_file             => "${ssl_dir}/certs/${fqdn}.crt",
+    #cert_chain_file       => "${ssl_dir}/certs/",
+    cert_type             => 'RSA',
+    additional_attributes => {
+      'SSLEnabled'          => bool2str($https_enabled),
+      'maxThreads'          => $https_connector_max_threads,
+      'scheme'              => $https_connector_scheme,
+      'secure'              => bool2str($https_connector_secure),
+      'clientAuth'          => bool2str($https_connector_client_auth),
+      'sslProtocol'         => $https_connector_ssl_protocol,
+      'sslEnabledProtocols' => join($https_connector_ssl_protocols_enabled, ','),
+      'ciphers'             => join($ciphers_enabled, ','),
 
-  #     'keystorePass'        => $keystore_pass.unwrap,
-  #     'keystoreFile'        => $keystore_path,
+      'keystorePass'        => $keystore_pass.unwrap,
+      'keystoreFile'        => $keystore_path,
+    },
+  }
 }
