bb89ec50f272a45d29713d6302ddd6e7e80c6cf9
Mihai Preda <mpreda@outlook.com>
2025-07-22 18:42:16 -0700

    added puppet server to puppet_agent

--- site/profile/manifests/puppet_agent.pp
+++ site/profile/manifests/puppet_agent.pp
@@ -3,6 +3,8 @@ class profile::puppet_agent {
   class { 'puppet_agent':
     package_version => '8.10.0',
     collection      => 'puppet8',
-    config          => [{ section => main, setting => runinterval, value => '1h' }],
+    config          => [{ section => main, setting => runinterval, value => '1h' },
+      { section=> main, setting => server, value => 'puppet.preda.ca' },
+    ],
   }
 }
