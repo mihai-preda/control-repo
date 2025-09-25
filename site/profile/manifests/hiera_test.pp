e04675d2d7d0f53e244e196e49eece9549230f40
Mihai Preda <mpreda@outlook.com>
2025-09-24 21:42:00 -0700

    added documentation for params as a test

--- site/profile/manifests/hiera_test.pp
+++ site/profile/manifests/hiera_test.pp
@@ -1,4 +1,8 @@
 # /etc/puppetlabs/code/environments/production/modules/profile/manifests/hiera_test.pp
+# @param ssl add to hiera as follows: profile::hiera_test::ssl: false or true
+# @param backups_enabled see below example
+# @param site_alias also see below. This documentation fixes mild error
+# showing when $vars don't have docu.
 class profile::hiera_test (
   Boolean             $ssl,
   Boolean             $backups_enabled,
