e422f2a145fad3d5c6a90f822123fd7484a6db5e
Mihai Preda <mpreda@outlook.com>
2025-09-27 16:42:05 -0700

    commented again the additional tomcat conf

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -29,18 +29,18 @@ class profile::tomcat {
     cert_file             => "${ssl_dir}/certs/${fqdn}.pem",
     cert_chain_file       => "${ssl_dir}/certs/ca.pem",
     cert_type             => 'EC',
-    additional_attributes => {
-      'SSLEnabled'          => bool2str($https_enabled),
-      'maxThreads'          => $https_connector_max_threads,
-      'scheme'              => $https_connector_scheme,
-      'secure'              => bool2str($https_connector_secure),
-      'clientAuth'          => bool2str($https_connector_client_auth),
-      'sslProtocol'         => $https_connector_ssl_protocol,
-      'sslEnabledProtocols' => join($https_connector_ssl_protocols_enabled, ','),
-      'ciphers'             => join($ciphers_enabled, ','),
+    # additional_attributes => {
+    #   'SSLEnabled'          => bool2str($https_enabled),
+    #   'maxThreads'          => $https_connector_max_threads,
+    #   'scheme'              => $https_connector_scheme,
+    #   'secure'              => bool2str($https_connector_secure),
+    #   'clientAuth'          => bool2str($https_connector_client_auth),
+    #   'sslProtocol'         => $https_connector_ssl_protocol,
+    #   'sslEnabledProtocols' => join($https_connector_ssl_protocols_enabled, ','),
+    #   'ciphers'             => join($ciphers_enabled, ','),
 
-      'keystorePass'        => $keystore_pass.unwrap,
-      'keystoreFile'        => $keystore_path,
-    },
+    #   'keystorePass'        => $keystore_pass.unwrap,
+    #   'keystoreFile'        => $keystore_path,
+    # },
   }
 }
