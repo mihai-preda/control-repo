d28aa71958c7ccd42622012201da62e767991944
Mihai Preda <mpreda@outlook.com>
2025-09-27 19:27:35 -0700

    used a different approach

--- site/profile/manifests/ssc.pp
+++ site/profile/manifests/ssc.pp
@@ -1,18 +1,18 @@
 # self-signed certificate
 class profile::ssc {
+  $fqdn = $facts['networking']['fqdn']
+  $ssl_dir = '/etc/pki/tls'
   class { 'openssl':
     package_ensure         => latest,
     ca_certificates_ensure => latest,
   }
   openssl::certificate::x509 { "%['networking']['fqdn']":
-    commonname => $facts['networking']['fqdn'],
+    commonname => $fqdn,
     country    => 'CA',
     state      => 'British Columbia',
     locality   => 'Delta',
     days       => 365,
   }
-  $fqdn = $facts['networking']['fqdn']
-  $ssl_dir = '/etc/pki/tls'
   openssl::export::pkcs12 { "${ssl_dir}/private/${fqdn}.p12":
     ensure    => 'present',
     basedir   => '/etc/pki/tls/',
