4285cc66e78a25866b8a81559630ef6c75d7d33f
Mihai Preda <mpreda@outlook.com>
2025-09-27 17:59:30 -0700

    added all vars and uncommented additional_attrs

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -11,37 +11,43 @@ class profile::tomcat {
   }
   $fqdn = $facts['networking']['fqdn']
   $ssl_dir = '/etc/pki/tls'
-  # $keystore_path = "${ssl_dir}/private/${fqdn}.p12"
-  # $keystore_source = "${ssl_dir}/private/${fqdn}.p12"
-  # $keystore_pass = '1k0eop3l0-2jd]3hh7'
-  # $keystore_user = 'tomcat'
-  # file { $keystore_path:
-  #   ensure   => file,
-  #   source   => $keystore_source,
-  #   owner    => $keystore_user,
-  #   mode     => '0400',
-  #   checksum => 'md5',
-  # }
+  $keystore_source = "${ssl_dir}/private/${fqdn}.p12"
+  $keystore_path = "${ssl_dir}/private/${fqdn}.p12"
+  $keystore_pass = '1k0eop3l0-2jd]3hh7'
+  $keystore_user = 'tomcat'
+  $https_enabled = true
+  $https_connector_max_threads = 200
+  $https_connector_scheme = 'https'
+  $https_connector_secure = true
+  $https_connector_client_auth = true
+  #$https_connector_ssl_protocol = 'TLSv1.2'+'TLSv1.3'
+  file { $keystore_path:
+    ensure   => file,
+    source   => $keystore_source,
+    owner    => $keystore_user,
+    mode     => '0400',
+    checksum => 'md5',
+  }
 
   -> tomcat::config::server::connector { 'default-https':
-    purge_connectors => true,
-    port             => '8443',
-    cert_key_file    => "${ssl_dir}/certs/${fqdn}.key",
-    cert_file        => "${ssl_dir}/certs/${fqdn}.pem",
-    cert_chain_file  => "${ssl_dir}/certs/ca.pem",
-    cert_type        => 'RSA',
-    # additional_attributes => {
-    #   'SSLEnabled'          => bool2str($https_enabled),
-    #   'maxThreads'          => $https_connector_max_threads,
-    #   'scheme'              => $https_connector_scheme,
-    #   'secure'              => bool2str($https_connector_secure),
-    #   'clientAuth'          => bool2str($https_connector_client_auth),
-    #   'sslProtocol'         => $https_connector_ssl_protocol,
-    #   'sslEnabledProtocols' => join($https_connector_ssl_protocols_enabled, ','),
-    #   'ciphers'             => join($ciphers_enabled, ','),
+    purge_connectors      => true,
+    port                  => '8443',
+    cert_key_file         => "${ssl_dir}/certs/${fqdn}.key",
+    cert_file             => "${ssl_dir}/certs/${fqdn}.pem",
+    cert_chain_file       => "${ssl_dir}/certs/ca.pem",
+    cert_type             => 'RSA',
+    additional_attributes => {
+      'SSLEnabled'   => bool2str($https_enabled),
+      'maxThreads'   => $https_connector_max_threads,
+      'scheme'       => $https_connector_scheme,
+      'secure'       => bool2str($https_connector_secure),
+      'clientAuth'   => bool2str($https_connector_client_auth),
+      'sslProtocol'  => $https_connector_ssl_protocol,
+      #'sslEnabledProtocols' => join($https_connector_ssl_protocols_enabled, ','),
+      #'ciphers'             => join($ciphers_enabled, ','),
 
-    #   'keystorePass'        => $keystore_pass.unwrap,
-    #   'keystoreFile'        => $keystore_path,
-    # },
+      'keystorePass' => $keystore_pass.unwrap,
+      'keystoreFile' => $keystore_path,
+    },
   }
 }
