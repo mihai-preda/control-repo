83548fe1aa0601fea4ea847d2fa9d2dd6d76332c
Mihai Preda <mpreda@outlook.com>
2025-10-04 21:57:42 -0700

    added ca cakey and cakeypass

--- site/profile/manifests/ssc.pp
+++ site/profile/manifests/ssc.pp
@@ -6,23 +6,25 @@ class profile::ssc {
     ca_certificates_ensure => latest,
   }
   openssl::certificate::x509 { $facts['networking']['fqdn']:
-    commonname => $facts['networking']['fqdn'],
-    country    => 'CA',
-    state      => 'British Columbia',
-    locality   => 'Delta',
-    days       => 365,
-    ca         => '/etc/pki/tls/certs/ca.pem',
+    commonname     => $facts['networking']['fqdn'],
+    country        => 'CA',
+    state          => 'British Columbia',
+    locality       => 'Delta',
+    days           => 365,
+    ca             => '/etc/pki/tls/certs/ca.pem',
+    cakey          => '/etc/pki/tls/certs/cakey.pem',
+    cakey_password => '1k0eop3l5-2jd]3hr7',
   }
   $fqdn = $facts['networking']['fqdn']
   $ssl_dir = '/etc/pki/tls'
   openssl::export::pkcs12 { $fqdn:
-    ensure   => 'present',
-    basedir  => "${ssl_dir}/certs/",
-    pkey     => "${ssl_dir}/certs/${fqdn}.key",
-    cert     => "${ssl_dir}/certs/${fqdn}.crt",
-    out_pass => '1k0eop3l0-2jd]3hh7',
+    ensure    => 'present',
+    basedir   => "${ssl_dir}/certs/",
+    pkey      => "${ssl_dir}/certs/${fqdn}.key",
+    cert      => "${ssl_dir}/certs/${fqdn}.crt",
+    out_pass  => '1k0eop3l6-2jd]3hy7',
     # the dynamic portion fails because the file resources are not declared in the catalogue
-    # dynamic   => true,
-    # resources => File["${ssl_dir}/certs/${fqdn}.key","${ssl_dir}/certs/${fqdn}.crt"],
+    dynamic   => true,
+    resources => File["${ssl_dir}/certs/${fqdn}.key","${ssl_dir}/certs/${fqdn}.crt"],
   }
 }
