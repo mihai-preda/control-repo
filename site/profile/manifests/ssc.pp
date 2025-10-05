e8f1c8403517ca28c825bd8c9b55e9669de66da0
Mihai Preda <mpreda@outlook.com>
2025-10-04 19:38:27 -0700

    removed onlyif from ssc

--- site/profile/manifests/ssc.pp
+++ site/profile/manifests/ssc.pp
@@ -21,7 +21,6 @@ class profile::ssc {
     pkey     => "${ssl_dir}/certs/${fqdn}.key",
     cert     => "${ssl_dir}/certs/${fqdn}.crt",
     out_pass => '1k0eop3l0-2jd]3hh7',
-    onlyif   => "/bin/test -f ${ssl_dir}/certs/${fqdn}.crt",
     # the dynamic portion fails because the file resources are not declared in the catalogue
     # dynamic   => true,
     # resources => File["${ssl_dir}/certs/${fqdn}.key","${ssl_dir}/certs/${fqdn}.crt"],
