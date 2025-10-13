116120e407c48356355b55450a1dd3ad1908f5b3
Mihai Preda <mpreda@outlook.com>
2025-10-13 09:07:06 -0700

    removed source from firewall

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -22,9 +22,8 @@ class profile::tomcat {
     war_source    => '/opt/tomcat/webapps/docs/appdev/sample/sample.war',
   }
   firewall { '101 allow http and https access':
-    dport  => [8081, 8443],
-    proto  => 'tcp',
-    jump   => 'accept',
-    source => '10.21.2.0/24',
+    dport => [8081, 8443],
+    proto => 'tcp',
+    jump  => 'accept',
   }
 }
