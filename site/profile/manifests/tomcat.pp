78c1addd190840311c2d799a747a8d16e647adb6
Mihai Preda <mpreda@outlook.com>
2025-10-12 22:31:25 -0700

    modified sample uri

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -18,7 +18,7 @@ class profile::tomcat {
     },
   }
   tomcat::war { 'sample.war':
-    catalina_base => '/opt/tomcat9/first',
-    war_source    => '/opt/tomcat9/webapps/docs/appdev/sample/sample.war',
+    catalina_base => '/opt/tomcat/first',
+    war_source    => '/opt/tomcat/webapps/docs/appdev/sample/sample.war',
   }
 }
