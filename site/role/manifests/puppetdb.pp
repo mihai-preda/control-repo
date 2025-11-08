80e2f8a539eb5a3cc71d419df4019b28a7fcd6df
Mihai Preda <mpreda@outlook.com>
2025-11-07 21:22:50 -0800

    edited the puppetdb role again

--- site/role/manifests/puppetdb.pp
+++ site/role/manifests/puppetdb.pp
@@ -1,9 +1,7 @@
 # role db
 class role::puppetdb {
-  include profile::default
-  include profile::puppet_agent
+  include role::default
   include profile::puppetdb
   include profile::zabbixdb
   include profile::resolver
-  include epel
 }
