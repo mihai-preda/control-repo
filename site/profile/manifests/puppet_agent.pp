a8cd3faed8a4d61ccc0555ec46e37e15f9629b36
Mihai Preda <mpreda@outlook.com>
2026-01-17 00:37:41 -0800

    modified puppet_agent

--- site/profile/manifests/puppet_agent.pp
+++ site/profile/manifests/puppet_agent.pp
@@ -1,10 +1,31 @@
 # profile puppet agent
-class profile::puppet_agent {
+# class puppet agent
+# @param puppet_server - the puppt master to point the agent to
+# @param run_interval - the time interval at which the puppet agent should run
+# @param environment - the code environment
+# @param agent_version - the agent version to be installed
+class profile::puppet_agent (
+  String $puppet_server = 'puppet.preda.ca',
+  String $run_interval  = '1h',
+  String $environment   = 'production',
+  String $agent_version = 'latest',
+) {
   class { 'puppet_agent':
-    package_version => '8.10.0',
+    package_version => $agent_version,
     collection      => 'puppet8',
-    config          => [{ section => main, setting => runinterval, value => '1h' },
-      { section=> main, setting => server, value => 'puppet.preda.ca' },
+    manage_repo     => true,
+    config          => [
+      {
+        section => 'main',
+        setting => 'server',
+        value   => $puppet_server,
+      },
+      {
+        section => 'main',
+        setting => 'runinterval',
+        value   => $run_interval,
+      },
+      # ... other settings
     ],
   }
 }
