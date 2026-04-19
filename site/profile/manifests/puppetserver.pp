3e45922b6bc7c52843518cc7d505e9c67e310e9c
Mihai Preda <mpreda@outlook.com>
2026-04-18 20:38:33 -0700

    removed sysconfig file from puppetserver

--- site/profile/manifests/puppetserver.pp
+++ site/profile/manifests/puppetserver.pp
@@ -3,12 +3,6 @@ class profile::puppetserver {
   package { 'puppetserver':
     ensure => 'installed',
   }
-  file { 'sysconfig-puppetserver':
-    ensure  => 'file',
-    path    => '/etc/sysconfig/puppetserver',
-    source  => '/puppet_course/files/sysconfig-puppetserver',
-    require => Package['puppetserver'],
-  }
   service { 'puppetserver':
     ensure  => true,
     enable  => true,
