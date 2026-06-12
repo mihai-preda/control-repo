c4238ccf769461455d8af047fd21ff102b740fe2
Mihai Preda <mpreda@outlook.com>
2026-06-11 22:26:10 -0700

    added certificate management to sp1 role

--- site/role/manifests/sp1.pp
+++ site/role/manifests/sp1.pp
@@ -1,6 +1,7 @@
 # role server physical 1
 class role::sp1 {
   include profile::base
+  include profile::certificates
   include profile::resolver
   include profile::openvox_agent
   include profile::zabbix_agent
